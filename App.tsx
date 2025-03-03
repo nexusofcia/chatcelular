399b3d2c574c149e26834351dfcec97b3c39045a
aurora <danielecristinnedias098@gmail.com>
2025-03-03 11:28:58 -0300

    inicio

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Toaster } from 'sonner-native';
+import HomeScreen from "./screens/HomeScreen";
+import PhoneAuthScreen from "./screens/PhoneAuthScreen";
+import ContactsListScreen from "./screens/ContactsListScreen";
+import ChatScreen from "./screens/ChatScreen";
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator screenOptions={{
+      headerShown: false
+    }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="PhoneAuth" component={PhoneAuthScreen} />
+      <Stack.Screen name="ContactsList" component={ContactsListScreen} />
+      <Stack.Screen name="ChatScreen" component={ChatScreen} options={{ headerShown: true }} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <SafeAreaProvider style={styles.container}>
+      <Toaster />
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
